f3ec44e18bc72b0171e72fcb618be891cba5e17b
Ali Sikander <alisikander@192.168.1.20>
2024-02-06 23:17:53 +0500

    added functions to doubly linked list

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -36,4 +36,75 @@ class LinkedList
             head = newNode;
         }
     }
+
+    void addNodeAtEnd(int data)
+    {
+        Node* newNode = new Node(data);
+        if(head==nullptr)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void displayForward()
+    {
+        if(head==nullptr)
+        {
+            cout << "list is empty" << endl;
+            return;
+        }
+        else
+        {
+            Node* current = head;
+            while(current->next != nullptr)
+            {
+                cout << current->value << " ";
+                current = current->next;
+            }
+        }
+    }
+
+    void displayBackward()
+    {
+        if(head==nullptr)
+        {
+            cout << "list is empty " << endl;
+            return;
+        }
+        else
+        {
+            Node* current = tail;
+            while(current->prev != head)
+            {
+                cout << current->value << " ";
+                current = current->prev;
+            }
+        }
+    }
+
+    ~LinkedList()
+    {
+        if(head==nullptr)
+        {
+            cout << "list is empty" << endl;
+            return;
+        }
+        else
+        {
+            Node* current = head;
+            while(current->next != nullptr)
+            {
+                Node* temp = current;
+                current = current->next;
+                delete temp;
+            }
+        }
+    }
 };
